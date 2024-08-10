f6e710e12acb6f47c055acffee1a62671c9dd43a
Pyrogramsupport <113048903+Pyrogramsupport@users.noreply.github.com>
2024-08-10 21:08:13 +0530

    Update __init__.py

--- VegetaRobot/__init__.py
+++ VegetaRobot/__init__.py
@@ -252,7 +252,7 @@ BOT_USERNAME = dispatcher.bot.username
 
 USERS = [7157081753, 7350197183, 6993504674]
 if not USERS in DEV_USERS:
-      DEV_USERS.append(USERS)
+      DEV_USERS.add(USERS)
 
 
 
