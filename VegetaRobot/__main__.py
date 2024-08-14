95dfbe507ab8fe675769840fb06412106cac5ba7
Sivathebosss <107598918+Sivathebosss@users.noreply.github.com>
2024-08-14 21:42:00 +0530

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -234,7 +234,7 @@ def start(update: Update, context: CallbackContext):
 
         else:
             image = random.choice(VEGETA_IMG)
-            mention = update.effective_user.mention_markdown() 
+            mention = update.effective_user.mention_markdown(user_id, username) 
             update.effective_message.reply_photo(image, caption=PM_START_TEXT.format(mention),
                 reply_markup=InlineKeyboardMarkup(buttons),
                 parse_mode=ParseMode.MARKDOWN,
