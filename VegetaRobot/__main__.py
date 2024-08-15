7b13fab71bfb81b7ae3549311b7500afa512eca3
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-08-15 17:48:38 +0530

    Update __main__.py

--- VegetaRobot/__main__.py
+++ VegetaRobot/__main__.py
@@ -235,7 +235,7 @@ def start(update: Update, context: CallbackContext):
         else:
             image = random.choice(VEGETA_IMG)
             user_id = update.effective_user.id
-            name = = update.effective_user.first_name
+            name =  update.effective_user.first_name
             mention = mention_markdown(user_id, name)
 
             update.effective_message.reply_photo(image, caption=PM_START_TEXT.format(mention),
